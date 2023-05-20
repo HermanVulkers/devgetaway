302cf28857de1745055c93d9b338258e70cbea8f
HERMAN VULKERS <hermanvulkers@gmail.com>
2023-05-20 17:17:18 +0200

    Revert "feat: add firebase ui login (not working"
    This reverts commit e1ad66d2cb3cd660943ffc01ae8eb1f76faf1423.

--- src/components/login-screen/account-buttons/account-buttons.tsx
+++ src/components/login-screen/account-buttons/account-buttons.tsx
@@ -1,24 +1,17 @@
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 import * as Styled from './account-buttons.style';
 
 export const AccountButtons = () => {
-    const router = useRouter();
-
-    const handleClick = async () => {
-        router.push('/authentication');
-    };
-
-    return (
-        <Styled.Container>
-            <Styled.Button
-                onClick={() => {
-                    handleClick();
-                }}
-            >
-                Sign In
-            </Styled.Button>
-        </Styled.Container>
-    );
+  return (
+    <Styled.Container>
+      <Styled.Button
+        onClick={() => {
+          signIn();
+        }}
+      >
+        Sign In
+      </Styled.Button>
+    </Styled.Container>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,43 +3,32 @@ import { LoginScreen } from '@/components/login-screen/login-screen';
 import { useSession } from 'next-auth/react';
 import { HomeScreen } from '@/components/home-screen/home-screen';
 
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '@/firebase/firebase';
-import { useEffect, useState } from 'react';
-
 export default function Home() {
-    const [user, setUser] = useState(null);
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User is signed in
-                const uid = user.uid;
-                console.log('uid', uid);
-                setUser(user); // set user state to user
-            } else {
-                // User is signed out
-                console.log('user is logged out');
-                setUser(null); // set user state to null
-            }
-        });
-    }, []);
+  const { status } = useSession();
 
-    return (
-        <>
-            <Head>
-                <title>devgetaway - explore</title>
-                <meta
-                    name="description"
-                    content="devgetaway - workspace exchange for software developers"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  const renderContent = () => {
+    if (status === 'loading') {
+      return <div>Loading...</div>;
+    } else if (status === 'authenticated') {
+      return <HomeScreen />;
+    } else {
+      return <LoginScreen />;
+    }
+  };
 
-            <main>{user ? <HomeScreen /> : <LoginScreen />}</main>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>devgetaway - explore</title>
+        <meta
+          name="description"
+          content="devgetaway - workspace exchange for software developers"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>{renderContent()}</main>
+    </>
+  );
 }
